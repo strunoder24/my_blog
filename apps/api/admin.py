b66db88a0d20e9db9de0738c2ba847ca72b28b78
Виктор Кондик <strunoder24@mail.ru>
2019-05-22 16:24:20 +1000

    Убрал джанговскую админку и добавил поле для распаршенного маркдауна

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -5,6 +5,8 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'lang', 'create_date', 'edit_date')
     list_display_links = ('id', 'title')
+    readonly_fields = ('author',)
+    exclude = ('parsed_markdown', 'markdown')
     list_per_page = 25
 
 
--- apps/api/models.py
+++ apps/api/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100, default='')
     markdown = models.TextField()
+    parsed_markdown = models.TextField(blank=True)
     lang = models.CharField(choices=[('en', 'english'), ('ru', 'russian')], default='ru', max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
